3f7b68f8ff5fcdd5bfd24e923a2b4920d8323811
renkefrerichs <frerichs.renke@gmx.net>
2024-08-28 17:48:08 +0200

    Update Umrechnen von Grundrechenarten.py

--- Umrechnen von Grundrechenarten.py
+++ Umrechnen von Grundrechenarten.py
@@ -1,64 +1,71 @@
-#Menü
-print("""Umrechnen von Grundrechenarten
+
+while True :
+    #Bedienoberfläche
+    print("""Umrechnen von Grundrechenarten
          
          (1) Umrechnen von cm in m
          (2) Umrechnen von mA in A
-         (3) Umrechnen von °C in F
+         (3) Umrechnen von °C in
          (4) Umrechnen von Km/h in m/s
          (5) Umrechnen von kg in g
-         """)
+             """)
 
-num10= float(input("Wähle deine Umrechnung  "))
+    num10= float(input("Wähle deine Umrechnung  "))
 
-#Funktion ungültige Eingabe
-if num10>5:
-    print("ungültige Eingabe starte den Prozess neu")
-if num10== 0:
-    print("ungültige Eingabe starte den Prozess neu")
 
-#Funktionen zur Berechnung bei Eingaben von 1-5
+    #Funktion ungültige Eingabe
     
-if num10== 1:
-    #Wertangabe für cm Umrechnung
-    num1= float(input("Gebe einen Wert in cm ein  "))
-    #Formel
-    Formel_m = (num1*pow( 10, -2 ))
-    #Ausgabe
-    print (num1, "cm", "sind", Formel_m, "Meter",)
+    if num10>5:
+        print("ungültige Eingabe starte den Prozess neu")
+    if num10== 0:
+        print("ungültige Eingabe starte den Prozess neu")
+        
+    #Funktionen zur Berechnung bei Eingaben von 1-5    
+    if num10== 1:
+        #Wertangabe für cm Umrechnung
+        num1= float(input("Gebe einen Wert in cm ein  "))
+        #Formel
+        Formel_m = (num1*pow( 10, -2 ))
+        #Ausgabe
+        print (f"\n{num1} cm sind {Formel_m} Meter")
     
-if num10== 2:
-    #Wertangabe für mA Umrechnung
-    num2 = float(input("Gib einen wert in mA an  "))
-    #Formel
-    Formel_mA = (num2*pow( 10, -3 ))
-    #Ausgabe
-    print(" ")
-    print (num2, "mA", "sind", Formel_mA, "A")
+    if num10== 2:
+        #Wertangabe für mA Umrechnung
+        num2 = float(input("Gib einen wert in mA an  "))
+        #Formel
+        Formel_mA = (num2*pow( 10, -3 ))
+        #Ausgabe
+        print (f"\n {num2} mA sind {Formel_mA} A")
 
-if num10== 3:
-    #Werteingabe für °C Umrechnung
-    num3 = float(input("Gib einen Wert in °C an  "))
-    #Formel
-    Formel_C = (num3*1.8+32)
-    #Ausgabe
-    print (" ")
-    print (num3, "°C", "sind", Formel_C, "Fahrenheit")
+    if num10== 3:
+       #Werteingabe für °C Umrechnung
+        num3 = float(input("Gib einen Wert in °C an  "))
+        #Formel
+        Formel_C = (num3*1.8+32)
+        #Ausgabe
+        print (f"\n {num3} °C sind Formel_C Fahrenheit")
     
-if num10== 4:
-    #Werteingabe für km/h Umrechnung
-    num4 = float(input("Gib einen Wert in km/h  "))
-    #Formel
-    Formel_m_s = (num4/3.6)
-    #Ausgabe
-    print (" ")
-    print (num4, "km/h", "sind", Formel_m_s, "m/s")
+    if num10== 4:
+        #Werteingabe für km/h Umrechnung
+        num4 = float(input("Gib einen Wert in km/h  "))
+        #Formel
+        Formel_m_s = (num4/3.6)
+        #Ausgabe
+        print (f"\n {num4} km/h sind {Formel_m_s} m/s")
     
-if num10== 5:
-    #Werteingabe für kg Umrechnung
-    num5 = float(input("Gib einen Wert in kg an  "  ))
-    #Formel
-    Formel_t =  (num5*pow(10, -3 ))
-    #Ausgabe
-    print(" ")
-    print (num5, "kg", "sind", Formel_t, "t")
-          
+    if num10== 5:
+        #Werteingabe für kg Umrechnung
+        num5 = float(input("Gib einen Wert in kg an  "  ))
+        #Formel
+        Formel_t =  (num5*pow(10, -3 ))
+        #Ausgabe
+        print (f"\n {num5} kg sind {Formel_t} t")
+        
+        #Neustart option
+    neustart=input("\nmöchtest du neustarten? (y/n): ")
+    if neustart == "n":
+        
+        break
+        
+    
+        
